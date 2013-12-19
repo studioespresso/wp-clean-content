d2a8f4e2d1441bb81a0bf23f27d39ce3788621a7
Jan Henckens <jan@onedge.be>
2013-12-19 18:08:06 +0100

    Categories in header clean up [touch: 15]

--- content.php
+++ content.php
@@ -11,6 +11,11 @@
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php clean_content_posted_on(); ?>
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+
+			<?php clean_content_meta_categories(); ?>
+			<?php endif; // End if 'post' == get_post_type() ?>
+
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -33,16 +38,6 @@
 
 	<footer class="entry-meta">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'clean-content' ) );
-				if ( $categories_list && clean_content_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'clean-content' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
 			<?php
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'clean-content' ) );
