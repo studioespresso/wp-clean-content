1e72e319d50dbb0c1a6b0baed5a775d067a273d3
Jan Henckens <jan@onedge.be>
2014-01-28 17:37:22 +0100

    .org submission fixes

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<?php do_action( 'clean_content_credits' ); ?>
 			<a href="http://wordpress.org/"><?php printf( __( 'Proudly powered by %s', 'clean-content' ), 'WordPress' ); ?></a>
- 			<span class="onedge"><?php printf( __( '%1$s by %2$s', 'clean-content' ), '<a href="http://on-edge.github.io/wp-clean-content/">Clean Content</a>', '<a href="http://onedge.be/" rel="designer">On Edge</a>' ); ?> <br />(Way of the future)</span>
+ 			<span class="onedge"><?php printf( __( '%1$s', 'clean-content' ), '<a href="http://on-edge.github.io/wp-clean-content/">Clean Content by On Edge</a>'); ?> <br />(Way of the future)</span>
 
  			
 		</div><!-- .site-info -->
